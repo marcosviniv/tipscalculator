3bb4786bc016fa433abc174cc153f109ff22a88f
Code Marcos <marcosv.code@gmail.com>
2024-08-17 17:39:33 -0300

    Start Project

---
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tipscalculator
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // Valor  total da conta
+    // Número de Pessoas
+    // Porcentagem da Gorjeta
+    // 10% , 15% ou 20%
+    // Calcular
+    // Limpar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
