a02c9db55bfe02dd660a2e4922170dd4f1d92c8c
Code Marcos <marcosv.code@gmail.com>
2024-08-18 15:30:45 -0300

    Retrieve radio buttons, calculate tips, and display the result.

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.tipscalculator
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.tipscalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -21,21 +25,84 @@ class MainActivity : AppCompatActivity() {
     // Limpar
 
     // Recuperar as Views do Layout
-        // Find view by id
     // View Binding
 
-    private lateinit var binding : ActivityMainBinding
+    // Recuperar radio buttons
+    // Calculo de Tips
+    // Mostrar resultado
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        binding.btnClean.setOnClickListener{
-            println("Marcos " + binding.tieTotal.text)
-            println("Marcos " + binding.tieNumberOfPeople.text)
+        var percentage: Int = 0
+
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 10
+            }
         }
 
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 15
+            }
         }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                percentage = 20
+            }
+        }
+
+        binding.btnClean.setOnClickListener {
+            binding.tvResult.text = ""
+            binding.tieTotal.setText("")
+            binding.spinerNumberOfPeople.setSelection(0)
+            binding.rbOptionOne.isChecked = false
+            binding.rbOptionTwo.isChecked = false
+            binding.rbOptionThree.isChecked = false
+        }
+
+        // Spinner + Adapter
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinerNumberOfPeople.adapter = adapter
+
+        var numOfPeopleSelected = 0
+        binding.spinerNumberOfPeople.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                numOfPeopleSelected = position
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?){}
+            }
+
+        binding.btnDone.setOnClickListener {
+
+            val totalTableTemp = binding.tieTotal.text
+
+            if (totalTableTemp?.isEmpty() == true) {
+                Snackbar.make(binding.tieTotal, "Preencha todos os campos", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val totalAmount: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Int = numOfPeopleSelected
+                val totalTemp = totalAmount / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                binding.tvResult.text = "Total with tips: $totalWithTips"
+            }
+
+        }
+
     }
+}
